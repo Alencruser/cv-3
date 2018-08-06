bdd73b0a896836a1ac6e1559485b02125a900ff3
Valentin Janvier <alencruserxx@hotmail.fr>
2018-08-06 16:44:58 +0200

    modif couleurs polices

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="custom.css">
 	<script src="https://unpkg.com/scrollreveal"></script>
 </head>
-<body class="container-fluid">
+<body>
 	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: black">
 		<a class="navbar-brand mx-auto" href="#comp">Compétences</a>
 		<a class="navbar-brand mx-auto" href="#exp">Expériences</a>
@@ -23,73 +23,73 @@
 			</div>
 		</div>
 	</div>
-	<div id="content" class="text-white">
-		<div class="comp important" id="comp">
-			<h2>Liste de compétences :</h2>
-			<div class="container">
-				<center><ul>
-					<li><h4>Maîtriser des outils de développement</h4></li>
-					<li><h4>Savoir modéliser des projets</h4></li>
-					<li><h4>Utiliser des framework</h4></li>
-					<li><h4>Repérer et corriger des problèmes sur un site</h4></li>
-				</ul></center>
+		<div id="content" class="text-white">
+			<div class="comp" id="comp">
+				<h2>Liste de compétences :</h2>
+				<div class="container important">
+					<center><ul>
+						<li><h4>Maîtriser des outils de développement</h4></li>
+						<li><h4>Savoir modéliser des projets</h4></li>
+						<li><h4>Utiliser des framework</h4></li>
+						<li><h4>Repérer et corriger des problèmes sur un site</h4></li>
+					</ul></center>
+				</div>
 			</div>
-		</div>
-		<script>ScrollReveal().reveal('.comp',{delay:200});</script>
-		<div class="exp important" id="exp">
-			<h2>Expériences professionnelles :</h2>
-			<div class="container">
-				<center><ul>
-					<li><h4>L'art du pain : 2014-2015 / Saint-Gaudens</h4>
-						<p>Apprenti boulanger</p></li>
-						<li><h4>L'épi d'or : 2015 / Saint-Lary-Soulan</h4>
+			<script>ScrollReveal().reveal('.comp',{delay:200});</script>
+			<div class="exp" id="exp">
+				<h2>Expériences professionnelles :</h2>
+				<div class="container important">
+					<center><ul>
+						<li><h4>L'art du pain : 2014-2015 / Saint-Gaudens</h4>
 							<p>Apprenti boulanger</p></li>
-							<li><h4>La boulangeoise : 2015-2016 / Saint-Gaudens</h4>
+							<li><h4>L'épi d'or : 2015 / Saint-Lary-Soulan</h4>
 								<p>Apprenti boulanger</p></li>
-							</ul></center>
+								<li><h4>La boulangeoise : 2015-2016 / Saint-Gaudens</h4>
+									<p>Apprenti boulanger</p></li>
+								</ul></center>
+							</div>
 						</div>
-					</div>
-					<script type="text/javascript">ScrollReveal().reveal('.exp',{delay:200});</script>
-					<div class="formation important" id="formation">
-						<h2>Formation(s)/Diplômes :</h2>
-						<div class="container">
-							<center><ul>
-								<li><h4>CFA du Comminges 2016 :</h4>
-									<p>CAP Boulanger</p></li>
-									<li><h4>Permis :</h4>
-										<p>Permis B + Véhicule</p></li>
-									</ul></center>
+						<script type="text/javascript">ScrollReveal().reveal('.exp',{delay:200});</script>
+						<div class="formation" id="formation">
+							<h2>Formation(s)/Diplômes :</h2>
+							<div class="container important">
+								<center><ul>
+									<li><h4>CFA du Comminges 2016 :</h4>
+										<p>CAP Boulanger</p></li>
+										<li><h4>Permis :</h4>
+											<p>Permis B + Véhicule</p></li>
+										</ul></center>
+									</div>
 								</div>
-							</div>
-							<script>ScrollReveal().reveal('.formation',{delay:200});</script>
-							<div class="contact important" id="contact">
-								<h2>Contact :</h2>
-								<div class="container">
-									<center><ul>
-										<li><h4>13 Allée de Couechot</h4></li>
-										<li><h4>31260 Mane</h4></li>
-										<li><h4>06 58 05 19 18</h4></li>
-										<li><h4>alencruserxx@hotmail.fr</h4></li>
-									</ul></center>
+								<script>ScrollReveal().reveal('.formation',{delay:200});</script>
+								<div class="contact " id="contact">
+									<h2>Contact :</h2>
+									<div class="container important">
+										<center><ul>
+											<li><h4>13 Allée de Couechot</h4></li>
+											<li><h4>31260 Mane</h4></li>
+											<li><h4>06 58 05 19 18</h4></li>
+											<li><h4>alencruserxx@hotmail.fr</h4></li>
+										</ul></center>
+									</div>
 								</div>
-							</div>
-							<script>ScrollReveal().reveal('.contact',{delay:200});</script>
-							<div class="interet important" id="interet">
-								<h2>Loisirs/Centre d'interêts :</h2>
-								<div class="container">
-									<center><ul>
-										<li><h4>Intelligence Artificielle</h4></li>
-										<li><h4>Psychologie</h4></li>
-										<li><h4>Déduction</h4></li>
-										<li><h4>/</h4></li>
-										<li><h4>Piano</h4></li>
-										<li><h4>Chant</h4></li>
-										<li><h4>Jeux-vidéos</h4></li>
-									</ul></center>
+								<script>ScrollReveal().reveal('.contact',{delay:200});</script>
+								<div class="interet" id="interet">
+									<h2>Loisirs/Centre d'interêts :</h2>
+									<div class="container important">
+										<center><ul>
+											<li><h4>Intelligence Artificielle</h4></li>
+											<li><h4>Psychologie</h4></li>
+											<li><h4>Déduction</h4></li>
+											<li><h4>/</h4></li>
+											<li><h4>Piano</h4></li>
+											<li><h4>Chant</h4></li>
+											<li><h4>Jeux-vidéos</h4></li>
+										</ul></center>
+									</div>
 								</div>
+								<script>ScrollReveal().reveal('.interet',{delay:200});</script>
 							</div>
-							<script>ScrollReveal().reveal('.interet',{delay:200});</script>
-						</div>
 						<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 						<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 						<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
