d1c3f438984c61b4c225eecc9c9f46ab513780b6
Valentin Janvier <alencruserxx@hotmail.fr>
2018-08-07 16:57:01 +0200

    modif placement choix des termes

--- cv.php
+++ cv.php
@@ -15,69 +15,69 @@
 			<img src="img/inst.jpg" width="75%" height="auto">
 		</div>
 		<div class="col-sm-8">
-			<h1 class="coh">Janvier</h1>
-			<h1>Valentin</h2>
-				<h3 class="important">Développeur Web Full-Stack Junior</h3>
-				<p>Développeur Junior en recherche de connaissances d'un maximum de techno pour être polyvalent, petit plus coté programmation.</p>
+			<h3 class="important">Développeur Web Full-Stack Junior</h3>
+			<p>Développeur Junior en recherche de connaissances d'un maximum de techno pour être polyvalent, petit plus coté programmation.</p>
+			<h2 class="title">Compétences</h2>
+			<img class="rounded float-right" src="img/simplon.png" width="30%" height="auto">
+			<p>Maîtriser des outils de développement <br><br>
+			Savoir modéliser des projets <br><br>
+			Utiliser des framework <br><br>
+			Repérer et corriger des problèmes sur un site</p>
+		</div>
+	</div>
+	<script>ScrollReveal().reveal('.base',{delay:300});</script>
+	<div class="row partone">
+		<div class="col-sm-4 darkest">
+			<div class="restart">
+				<p class="icon"><i class="fas fa-male"></i>Janvier Valentin</p>
+				<p class="icon"><i class="fas fa-birthday-cake"></i>Née le 04/02/1997 (21 ans)</p>
+				<p class="icon"><i class="fas fa-car"></i>Permis B + véhicule</p>
+				<p class="icon"><i class="fas fa-envelope"></i>Alencruserxx@hotmail.fr</p>
+				<p class="icon"><i class="fas fa-phone"></i> 06 58 05 19 18</p>
+				<p class="icon"><i class="fas fa-map-marker-alt"></i>13 Allée de Couechot 31260 Mane</p>
+				<p class="icon"><i class="fas fa-globe-americas"></i>https://github.com/Alencruser</p>
 			</div>
 		</div>
-		<script>ScrollReveal().reveal('.base',{delay:300});</script>
-		<div class="row partone">
-			<div class="col-sm-4 darkest">
-				<h2>Contact</h2>
-				<div class="restart">
-					<p class="icon"><i class="fas fa-envelope"></i>Alencruserxx@hotmail.fr</p>
-					<p class="icon"><i class="fas fa-phone"></i> 06 58 05 19 18</p>
-					<p class="icon"><i class="fas fa-map-marker-alt"></i>13 Allée de Couechot 31260 Mane</p>
-					<p class="icon"><i class="fas fa-globe-americas"></i>https://github.com/Alencruser</p>
-				</div>
-			</div>
-			<div class="col-sm-8">
-				<h2 class="title">Expérience de travail</h2>
-				<ul>
-					<li><p>L'art du pain : 2014-2015 / Saint-Gaudens</p>
+		<div class="col-sm-8">
+			<h2 class="title">Expériences professionnelles</h2>
+			<ul>
+				<li><p>L'art du pain : 2014-2015 / Saint-Gaudens</p>
+					<p>Apprenti boulanger</p></li>
+					<li><p>L'épi d'or : 2015 / Saint-Lary-Soulan</p>
 						<p>Apprenti boulanger</p></li>
-						<li><p>L'épi d'or : 2015 / Saint-Lary-Soulan</p>
+						<li><p>La boulangeoise : 2015-2016 / Saint-Gaudens</p>
 							<p>Apprenti boulanger</p></li>
-							<li><p>La boulangeoise : 2015-2016 / Saint-Gaudens</p>
-								<p>Apprenti boulanger</p></li>
-							</ul>
-						</div>
+						</ul>
 					</div>
-					<script>ScrollReveal().reveal('.partone',{delay:400});</script>
-					<div class="row parttwo">
-						<div class="col-sm-4 darkest">
-							<h2>Compétences</h2>
-							<div class="restart">
-								<p>Maîtriser des outils de développement</p>
-								<p>Savoir modéliser des projets</p>
-								<p>Utiliser des framework</p>
-								<p>Repérer et corriger des problèmes sur un site</p>
-							</div>
-						</div>
-						<div class="col-sm-8">
-							<h2 class="title">Formation(s)</h2>
-							<ul>
+				</div>
+				<script>ScrollReveal().reveal('.partone',{delay:400});</script>
+				<div class="row parttwo">
+					<div class="col-sm-4 darkest">
+						<img class="logo" src="img/simplon.png" width="65%" height="auto">
+					</div>
+					<div class="col-sm-8">
+						<h2 class="title">Formation(s)</h2>
+						<ul>
+							<li><h4>Simplon St-Gaudens Promo 2018-2019</h4>
+								<p>Développeur Logiciel Niveau III</p></li>
 								<li><h4>CFA du Comminges 2016 :</h4>
 									<p>CAP Boulanger</p></li>
-									<li><h4>Permis :</h4>
-										<p>Permis B + Véhicule</p></li>
-									</ul>
-									<h2 class="title">Loisirs</h2>
-									<ul>
-										<li><h4>Intelligence Artificielle</h4></li>
-										<li><h4>Psychologie</h4></li>
-										<li><h4>Déduction</h4></li>
-										<li><h4>/</h4></li>
-										<li><h4>Piano</h4></li>
-										<li><h4>Chant</h4></li>
-										<li><h4>Jeux-vidéos</h4></li>
-									</ul>
-								</div>
+								</ul>
+								<h2 class="title">Loisirs</h2>
+								<ul>
+									<li><h4>Intelligence Artificielle</h4></li>
+									<li><h4>Psychologie</h4></li>
+									<li><h4>Déduction</h4></li>
+									<li><h4>/</h4></li>
+									<li><h4>Piano</h4></li>
+									<li><h4>Chant</h4></li>
+									<li><h4>Jeux-vidéos</h4></li>
+								</ul>
 							</div>
-							<script>ScrollReveal().reveal('.parttwo',{delay:500});</script>
-							<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-							<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-							<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-						</body>
-						</html>
+						</div>
+						<script>ScrollReveal().reveal('.parttwo',{delay:500});</script>
+						<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+						<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+						<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+					</body>
+					</html>
