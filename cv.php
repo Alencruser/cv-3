ef76ca9000c6bd5008f949c7a82070e29743a3b8
Valentin Janvier <alencruserxx@hotmail.fr>
2018-08-07 15:53:41 +0200

    ajout cv version simple

---
+++ cv.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Curriculum Vitae</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="custom.css">
+	<script src="https://unpkg.com/scrollreveal"></script>
+</head>
+<body class="container-fluid">
+	<div class="row base">
+		<div class="col-sm-4 darkest">
+			<img src="img/inst.jpg" width="75%" height="auto">
+		</div>
+		<div class="col-sm-8">
+			<h1 class="coh">Janvier</h1>
+			<h1>Valentin</h2>
+				<h3 class="important">Développeur Web Full-Stack Junior</h3>
+				<p>Développeur Junior en recherche de connaissances d'un maximum de techno pour être polyvalent, petit plus coté programmation.</p>
+			</div>
+		</div>
+		<script>ScrollReveal().reveal('.base',{delay:300});</script>
+		<div class="row partone">
+			<div class="col-sm-4 darkest">
+				<h2>Contact</h2>
+				<div class="restart">
+					<p class="icon"><i class="fas fa-envelope"></i>Alencruserxx@hotmail.fr</p>
+					<p class="icon"><i class="fas fa-phone"></i> 06 58 05 19 18</p>
+					<p class="icon"><i class="fas fa-map-marker-alt"></i>13 Allée de Couechot 31260 Mane</p>
+					<p class="icon"><i class="fas fa-globe-americas"></i>https://github.com/Alencruser</p>
+				</div>
+			</div>
+			<div class="col-sm-8">
+				<h2 class="title">Expérience de travail</h2>
+				<ul>
+					<li><p>L'art du pain : 2014-2015 / Saint-Gaudens</p>
+						<p>Apprenti boulanger</p></li>
+						<li><p>L'épi d'or : 2015 / Saint-Lary-Soulan</p>
+							<p>Apprenti boulanger</p></li>
+							<li><p>La boulangeoise : 2015-2016 / Saint-Gaudens</p>
+								<p>Apprenti boulanger</p></li>
+							</ul>
+						</div>
+					</div>
+					<script>ScrollReveal().reveal('.partone',{delay:400});</script>
+					<div class="row parttwo">
+						<div class="col-sm-4 darkest">
+							<h2>Compétences</h2>
+							<div class="restart">
+								<p>Maîtriser des outils de développement</p>
+								<p>Savoir modéliser des projets</p>
+								<p>Utiliser des framework</p>
+								<p>Repérer et corriger des problèmes sur un site</p>
+							</div>
+						</div>
+						<div class="col-sm-8">
+							<h2 class="title">Formation(s)</h2>
+							<ul>
+								<li><h4>CFA du Comminges 2016 :</h4>
+									<p>CAP Boulanger</p></li>
+									<li><h4>Permis :</h4>
+										<p>Permis B + Véhicule</p></li>
+									</ul>
+								</div>
+							</div>
+							<script>ScrollReveal().reveal('.parttwo',{delay:300});</script>
+							<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+							<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+							<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+						</body>
+						</html>
